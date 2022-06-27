69e3f315b621fe01d8ea31764cb93b7213b01c0c
millind123 <millind2611@gmail.com>
2022-06-27 13:53:21 +0530

    problemset page and problem page for each problem added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Problems
 
+def problem_statement(request,number):    
+    problemset = Problems.objects.all()
+    for p in problemset:
+        if p.problem_number == number:
+            problem=p
+    # problem=problemset[0]
+    context = {
+        'problem':problem
+    }
+    return render(request, 'polls/problemStatement.html',context)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    problemset=Problems.objects.all();
+    context = {
+        'problemset':problemset
+    }
+    return render(request,'polls/index.html',context)
